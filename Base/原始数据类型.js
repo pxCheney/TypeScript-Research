48ef445c9edae8bc3fe3d4a4ee5b6f74d0fd06b1
pxCheney <18511293107@163.com>
2020-06-22 18:19:53 +0800

    [feat] base

---
+++ Base/原始数据类型.js
@@ -0,0 +1,26 @@
+/**
+  原始数据类型
+
+  JavaScript 的类型分为两种：原始数据类型 和 对象类型。
+  原始数据类型包括：布尔值、数值、字符串、null、undefined 以及 ES6 中的新类型 Symbol。
+*/
+// // 1. 布尔值
+// let isDone: boolean = false;
+// let isDoneByBoolean: boolean = Boolean(1)
+// console.log('boolean', isDone, isDoneByBoolean) // false true
+// new Boolean() 生成的是对象，回=会报错
+// let isDoneByNewBoolean: boolean = new boolean(1)
+// 2. 数值
+// let number: number = 2
+// console.log('nember', number) // 2
+// 3. 字符串
+// let username: string = '张三'
+// console.log('string', username)
+// 4. null 和 undefined
+// undefined 和 null 是所有类型的子类型。也就是说 undefined 类型的变量，可以赋值给 number 类型的变量：
+// 当你指定了--strictNullChecks标记，null和undefined只能赋值给void和它们各自。
+var n = null;
+var u = undefined;
+var num = undefined;
+// console.log('null-undefined', num, n, u)
+// 5. 空值
